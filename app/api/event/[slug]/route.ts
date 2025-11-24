38c04dd368b17a5d174faadf8c0b64689c5cf753
Sahan Chathura <73782580+SanthaKudu47@users.noreply.github.com>
2025-11-25 05:46:07 +0900

    feat:implemented event upload route handler

--- app/api/event/[slug]/route.ts
+++ app/api/event/[slug]/route.ts
@@ -1,8 +1,12 @@
 import { EventModel } from "@/database/event.model";
 import dbConnect from "@/database/mongodb";
+import { uploadImage } from "@/lib/imageUploader";
 import { sendResponse } from "@/lib/response";
+import { validateEventData } from "@/lib/valiadation/schemas.event";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 import z from "zod";
+import cloudinary from "cloudinary";
 
 type RouteParams = {
   params: Promise<{ slug: string }>;
@@ -76,4 +80,147 @@ export async function GET(request: NextRequest, { params }: RouteParams) {
       errors: null,
     });
   }
+}
+
+export async function PUT(request: NextRequest, { params }: RouteParams) {
+  const headersLIst = await headers();
+  const contentType = headersLIst.get("content-type") || "";
+
+  if (!contentType.includes("multipart/form-data")) {
+    return sendResponse({
+      success: false,
+      message: "Validation Failed",
+      data: null,
+      errors: {
+        contentType: "Invalid Content-Type. Expected multipart/form-data.",
+      },
+      status: 400, // Unsupported Media Type
+    });
+  }
+
+  const formData = await request.formData();
+  let zodErrorMessages = null;
+  const parsedResult = validateEventData(formData);
+
+  if (!parsedResult.success || !parsedResult.data) {
+    const error = parsedResult.error;
+    const flattened = z.flattenError(error);
+    zodErrorMessages = flattened.fieldErrors;
+
+    return sendResponse({
+      success: false,
+      message: "Validation failed",
+      data: null,
+      errors: zodErrorMessages,
+      status: 400,
+    });
+  }
+
+  //
+  try {
+    await dbConnect();
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    return sendResponse({
+      success: false,
+      message: "Unable to connect to database",
+      errors: null,
+      data: null,
+      status: 500,
+    });
+  }
+
+  const eventData = parsedResult.data;
+  const slug = eventData.slug;
+  const newImage = eventData.image;
+  let existingEvent: {
+    _id: string;
+    image: string;
+  } | null = null;
+  //get existing event
+
+  try {
+    existingEvent = await EventModel.findOne(
+      {
+        slug: slug,
+      },
+      "_id image"
+    );
+  } catch (error) {
+    console.error("Failed to fetch event:", error);
+    return sendResponse({
+      success: false,
+      message: "Database query failed",
+      data: null,
+      status: 400,
+    });
+  }
+
+  if (!existingEvent) {
+    return sendResponse({
+      success: false,
+      message: "Event not found",
+      data: null,
+      status: 404,
+    });
+  }
+
+  let updatedImageUrl = existingEvent.image; //default will be current
+  if (newImage) {
+    let uploadResult: cloudinary.UploadApiResponse | undefined = undefined;
+
+    try {
+      uploadResult = await uploadImage(newImage, {
+        overwrite: true,
+        existingUrl: existingEvent.image,
+      });
+      if (!uploadResult) {
+        console.error("Upload result missing");
+        return sendResponse({
+          success: false,
+          message: "Image upload failed",
+          data: null,
+          status: 500,
+        });
+      }
+
+      updatedImageUrl = uploadResult.secure_url;
+    } catch (error) {
+      console.error("Cloudinary upload failed:", error);
+      return sendResponse({
+        success: false,
+        message: "Unable to upload image",
+        data: null,
+        status: 502,
+      });
+    }
+  }
+
+  //
+
+  //handle image upload...
+
+  let updatedDoc;
+  try {
+    updatedDoc = await EventModel.findByIdAndUpdate(
+      existingEvent._id,
+      { ...eventData, image: updatedImageUrl },
+      { new: true }
+    );
+  } catch (error) {
+    console.error("Event update failed:", error);
+    return sendResponse({
+      success: false,
+      message: "Database update failed",
+      data: null,
+      status: 400,
+    });
+  }
+
+  return sendResponse({
+    status: 200,
+    success: true,
+    message: "Event updated successfully",
+    data: updatedDoc,
+  });
 }
